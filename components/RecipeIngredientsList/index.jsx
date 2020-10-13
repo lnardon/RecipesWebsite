91f737d91d298f0095f790f74538eb07ee025b00
lnardon <lucaslnardon@gmail.com>
2020-10-13 03:10:44 -0300

    🚧 Created basic ingredients list for recipe page

---
+++ components/RecipeIngredientsList/index.jsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css";
+
+function RecipeIngredientsList({ ingredients }) {
+  return (
+    <div className={styles.ingredientsListContainer}>
+      {ingredients.map((ingredient) => {
+        return (
+          <div className={styles.ingredientContainer}>
+            <h1 className={styles.ingredientName}>{ingredient.name}</h1>
+            <h2>
+              {ingredient.amount} {ingredient.measures.metric.unitShort}
+            </h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default RecipeIngredientsList;
--- components/RecipePageHeader/index.jsx
+++ components/RecipePageHeader/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function RecipePageHeader({ image, title, summary }) {
   return (
-    <div className="recipePageHeaderContainer">
-      <img src={image} alt="Cover" />
+    <div className="recipePageHeaderContainer" style={{ width: "100%" }}>
+      <img src={image} alt="Cover" style={{ width: "100%" }} />
       <div>
         <h1>{title}</h1>
         <p>{summary}</p>
