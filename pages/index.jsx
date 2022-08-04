c7e81e4e50c63aba5507c089da9ff0af46e7f5c7
Lucas Nardon <lucaslnardon@gmail.com>
2022-08-04 03:19:04 -0300

    ✨ Add random recipe fetch

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,20 @@
 import Head from "next/head";
+import Router from "next/router";
 
 import Header from "../components/Header";
 
 export default function Home() {
+  function fetchRandomRecipe() {
+    fetch("/api/getRandomRecipe")
+      .then((res) => res.json())
+      .then((data) => {
+        Router.push(`/recipe/${data.recipes[0].id}`);
+      });
+  }
   return (
     <>
       <Head>
-        <title>Homepage</title>
+        <title>ReciPeas Website</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
           rel="stylesheet"
@@ -14,7 +22,12 @@ export default function Home() {
       </Head>
       <div>
         <Header />
-        <h1>Recipe Website</h1>
+        <h1>ReciPeas Website</h1>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/5501/5501234.png"
+          alt="Logo"
+        />
+        <button onClick={fetchRandomRecipe}>Get Random Recipe</button>
       </div>
     </>
   );
