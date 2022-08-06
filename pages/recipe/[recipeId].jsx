1faec2e0715bd580804875a8debec06202471d17
Lucas Nardon <lucaslnardon@gmail.com>
2022-08-06 04:13:25 -0300

    💄 Update recipe page UI

--- pages/recipe/[recipeId].jsx
+++ pages/recipe/[recipeId].jsx
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import Router, { useRouter } from "next/router";
 
-import RecipePageHeader from "../../components/RecipePageHeader";
-import RecipeIngredientsList from "../../components/RecipeIngredientsList";
+import IngredientsList from "../../components/IngredientsList";
 import ExpandableArea from "../../components/ExpandableArea";
+import LoadingFallback from "../../components/LoadingFallback";
+import getRandomInt from "../../hooks/getRandomInt";
 import styles from "./styles.module.css";
 
 function RecipePage({ recipe }) {
   const { isFallback } = useRouter();
 
   if (isFallback) {
-    return <h1>loading</h1>;
+    return <LoadingFallback />;
   }
 
   return (
@@ -21,24 +22,27 @@ function RecipePage({ recipe }) {
       <div className={styles.container}>
         <div className={styles.content}>
           <h4 onClick={() => Router.back()} className={styles.backBtn}>
-            go back
+            back
           </h4>
-          <RecipePageHeader image={recipe.image} title={recipe.title} />
+          <div className="" style={{ width: "100%", maxWidth: 1024 }}>
+            <img src={recipe.image} alt="Cover" className={styles.image} />
+            <h1 className={styles.title}>{recipe.title}</h1>
+          </div>
 
           <ExpandableArea
-            title="Ingredients"
+            title={`Ingredients (${recipe.extendedIngredients.length})`}
             content={
-              <RecipeIngredientsList ingredients={recipe.extendedIngredients} />
+              <IngredientsList ingredients={recipe.extendedIngredients} />
             }
           />
           <ExpandableArea
             title="Instructions"
             content={
-              <div>
+              <div className={styles.stepsContainer}>
                 {recipe?.analyzedInstructions[0]?.steps.map((instruction) => {
                   return (
-                    <div style={{ padding: "0rem 1rem" }}>
-                      <h2>
+                    <div className={styles.stepContainer}>
+                      <h2 className={styles.step}>
                         {instruction.number} - {instruction.step}
                       </h2>
                     </div>
@@ -69,10 +73,25 @@ export async function getStaticProps(context) {
       body: recipeId,
     }
   );
-  const parsedResponse = await response.json();
+  let parsedResponse = await response.json();
+  // If recipe ID doesn't exist, try another one
+  if (parsedResponse.code === 404) {
+    const response = await fetch(
+      "https://recipes-website.lnardon.vercel.app/api/getRecipeInfo",
+      {
+        method: "POST",
+        body: getRandomInt(1, 1650000),
+      }
+    );
+    parsedResponse = await response.json();
+    return {
+      props: { recipe: parsedResponse },
+      revalidate: 24 * 60 * 60, // 24 hours
+    };
+  }
   return {
     props: { recipe: parsedResponse },
-    revalidate: 10,
+    revalidate: 24 * 60 * 60, // 24 hours
   };
 }
 
